8172932af581b508acbc259415ebb3503f379925
abeiro <abeiro>
2008-01-23 12:15:31 +0000

    * Remove a debug statment

--- ascore/Apps/Bilo/list.php
+++ ascore/Apps/Bilo/list.php
@@ -6,7 +6,6 @@ if(BILO_isOperator()){
 	$u=newObject("user");
 	$u->isAdmin=BILO_isAdmin();
 	$u->searchResults=$u->selectAll($offset,$sort);
-	dataDump($u);
 	listList($u,array("grupos_nombre"=>"fref#user|ID|listGroupsNames"),"list_users","",1,"plParseTemplateFast");
 
 }else
